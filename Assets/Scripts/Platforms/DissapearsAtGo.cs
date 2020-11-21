53470f935d29c487e6e90f2e4b0cdd5f493b22c9
Nathan PM <nathan.pm96@gmail.com>
2020-11-22 00:34:05 +0100

    Script plataforma que desaparece al irte

---
+++ Assets/Scripts/Platforms/DissapearsAtGo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissapearsAtGo : MonoBehaviour
+{
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //Cambiar por Shader Dissolve y desactivar collider
+            gameObject.SetActive(false);
+        }
+    }
+
+}
