102a531e314e31e7a3993644106f7a891eb9fe0a
David <davisneira@gmail.com>
2020-11-21 14:59:31 +0100

    Primer prototipo de la función de traduccion y UI

---
+++ Assets/UI/Scripts/GlobalLanguage.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalLanguage : MonoBehaviour
+{
+    public static string CurrentLanguage = "English";
+    public Toggle toggleEnglish;
+    public Toggle toggleSpanish;
+    public Toggle toggleGalician;
+    public bool isInMenu;
+
+    void Awake()
+    {
+        //DontDestroyOnLoad(transform.gameObject);
+    }
+
+    public string ReturnLanguage()
+    {
+        return (CurrentLanguage);
+    }
+
+    private void Start()
+    {
+        ChangeLanguage(CurrentLanguage);
+        print(CurrentLanguage);
+    }
+
+    public void ChangeLanguage (string language)
+    {
+        CurrentLanguage = language;
+        NotificationCenter.DefaultCenter().PostNotification(this, "ChangeLanguage_P");
+
+        if (isInMenu)
+        {
+            if(language == "Spanish")
+            {
+                toggleSpanish.isOn = true;
+            }
+            if (language == "English")
+            {
+                toggleEnglish.isOn = true;
+            }
+            if (language == "Galician")
+            {
+                toggleGalician.isOn = true;
+            }
+        }
+    }
+}
---
+++ Assets/UI/Scripts/Language.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Language : MonoBehaviour
+{
+
+    public string spanish;
+    public string english;
+    public string galician;
+    public bool isButton;
+    public Toggle toggleSpanish;
+    public Toggle toggleEnglish;
+    public Toggle toggleGalician;
+
+    public GlobalLanguage globalLanguage;
+
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        globalLanguage = GameObject.Find("LanguageManager").GetComponent<GlobalLanguage>();
+    }
+    void Start()
+    {
+        NotificationCenter.DefaultCenter().AddObserver(this, "ChangeLanguage_P");
+        ChangeLanguage_P();
+    }
+
+    public void ChangeLanguage_P ()
+    {
+        if (globalLanguage.ReturnLanguage () == "Spanish")
+        {
+            if (isButton)
+            {
+                GetComponentInChildren<Text>().text = spanish;
+            }
+            else
+            {
+                GetComponent<Text>().text = spanish;
+            }
+        }
+        if (globalLanguage.ReturnLanguage() == "English")
+        {
+            if (isButton)
+            {
+                GetComponentInChildren<Text>().text = english;
+            }
+            else
+            {
+                GetComponent<Text>().text = english;
+            }
+        }
+        if (globalLanguage.ReturnLanguage() == "Galician")
+        {
+            if (isButton)
+            {
+                GetComponentInChildren<Text>().text = galician;
+            }
+            else
+            {
+                GetComponent<Text>().text = galician;
+            }
+        }
+    }
+}
