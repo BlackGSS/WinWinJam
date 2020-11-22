3d4c49e48ee7c598a3d3c7e9678c9d941aa5bd5d
Nathan PM <nathan.pm96@gmail.com>
2020-11-22 17:28:53 +0100

    Casi final chavales

--- Assets/Scripts/VictoryBehaviour/VictoryManager.cs
+++ Assets/Scripts/VictoryBehaviour/VictoryManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class VictoryManager : MonoBehaviour
 {
@@ -20,18 +21,38 @@ public class VictoryManager : MonoBehaviour
 	private void VictoryAchieved(int victoryID)
 	{
 
-		Instantiate(resetGame);
+		resetGame.SetActive(true);
 
 		if (player.bootsEquipment)
 			victoryID = 1;
 
 		if (GlobalLanguage.CurrentLanguage == "English")
-			ToastManager.Instance.SetText(victories[victoryID].textWinEng, 20f);
+			ToastManager.Instance.SetText(victories[victoryID].textWinEng, 40f);
 
 		if (GlobalLanguage.CurrentLanguage == "Spanish")
-			ToastManager.Instance.SetText(victories[victoryID].textWinSpa, 20f);
+			ToastManager.Instance.SetText(victories[victoryID].textWinSpa, 40f);
 
 		if (GlobalLanguage.CurrentLanguage == "Galician")
-			ToastManager.Instance.SetText(victories[victoryID].textWinGal, 20f);
+			ToastManager.Instance.SetText(victories[victoryID].textWinGal, 40f);
+
+		StartCoroutine(EndGame());
+	}
+
+	public void ResetGame()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().ToString());
+	}
+
+	public void ReturnToMenu()
+	{
+		SceneManager.LoadScene("Menu");
+	}
+
+	IEnumerator EndGame()
+	{
+		yield return new WaitForSeconds(40f);
+
+		//SceneManager.LoadScene("Menu");
+
 	}
 }
