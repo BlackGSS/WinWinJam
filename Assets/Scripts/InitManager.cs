40dce269cef59ac5364e079fdc249810fae3d143
Nathan PM <nathan.pm96@gmail.com>
2020-11-22 14:45:25 +0100

    InitManager

---
+++ Assets/Scripts/InitManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Experimental.Rendering.Universal;
+using System;
+
+public class InitManager : MonoBehaviour
+{
+    public VolumeProfile newProfile;
+
+    public static Action onHeadTake = () => { };
+
+    private void Start()
+    {
+        onHeadTake += Taken;
+    }
+
+    private void Taken()
+    {
+        GetComponent<Volume>().profile = newProfile;
+    }
+}
